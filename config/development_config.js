e2353a7ad946852c1d54592c815f98a5102ee2ef
marklin <marklin@nctu.edu.tw>
2020-08-06 17:20:30 +0800

    ver.1 after deployment onto Heroku

--- config/development_config.js
+++ config/development_config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
     mysql: {
-      host: process.env.HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE
+      host: 'us-cdbr-east-02.cleardb.com',
+      user: 'bcf827ce5d43a8',
+      password: '09196a9c',
+      database: 'heroku_1ec596bb0f3974f'
     },
     secret: process.env.MY_SECRET
 }

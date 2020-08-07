bd7ee823e9c555ca351f4165a42d6cbf17c42321
marklin <marklin@nctu.edu.tw>
2020-08-07 12:03:38 +0800

    ver.3 after deployment onto Heroku

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var product = require('./routes/product');
 var member= require('./routes/member');
 var order = require('./routes/order');
 
+var cors = require('cors');
 var app = express();
 app.use(cors());
 
